540ab9ff31954f5bb0ab945172d76b57b538db91
antonio.decaro <antonio.decaro@outlook.it>
2019-12-18 17:19:59 +0100

    Aggiunta l'implementazione delle interfacce DAO che accedono al DataBase.

--- src/main/java/model/database/package-info.java
+++ src/main/java/model/database/package-info.java
@@ -2,7 +2,3 @@
  * Questo package fornisce le classi attraverso le quali effettuare l'accesso al DataBase.
  */
 package model.database;
-
-
-
-//TODO aggiungere l'implementazione concreta dei DAO.
--- src/main/java/model/package-info.java
+++ src/main/java/model/package-info.java
@@ -1,5 +1,5 @@
 
 /**
- * Questo package definisce l'insieme delle classi e delle interfacce che costituiscono la logica di buisness del nostro software.
+ * Questo package definisce l'insieme delle classi e delle interfacce che costituiscono la logica di buisness del sistema.
  * */
 package model;
