8595e0fefdf4a619864f19b5a8971cb95b5b8768
Marco <marcodesantis0198@gmail.com>
2019-12-23 20:32:22 +0100

    Implementation LogoutServletTest.java

--- src/main/java/control/autenticazione/LogoutServlet.java
+++ src/main/java/control/autenticazione/LogoutServlet.java
@@ -20,7 +20,7 @@ public class LogoutServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         request.getSession().invalidate();
-        response.sendRedirect(request.getServletContext().getContextPath() + "/index.jsp");
+        response.sendRedirect("/index.jsp");
     }
 
     @Override
--- src/test/control/autenticazione/LogoutServletTest.java
+++ src/test/control/autenticazione/LogoutServletTest.java
@@ -15,32 +15,31 @@ import org.mockito.Mockito;
 class LogoutServletTest  extends Mockito {
 
     @Mock
-    HttpServletRequest request;
+    private HttpServletRequest request;
     @Mock
-    HttpServletResponse response;
+    private HttpServletResponse response;
     @Mock
-    HttpSession session;
+    private HttpSession session;
 
     @Mock
-    RequestDispatcher rd;
+    private RequestDispatcher rd;
 
     /**
-     * Questo metodo testa ServletLogout nel caso in cui si vuole effettuare il
+     * Questo metodo testa LogoutServlet nel caso in cui si vuole effettuare il
      * logout.
      */
 
     @Test
-    public void testServletLogout() throws Exception {
+    public void testLogoutServlet() throws Exception {
 
         HttpServletRequest request = mock(HttpServletRequest.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
         HttpSession session = mock(HttpSession.class);
-        // RequestDispatcher rd=mock(RequestDispatcher.class);
 
         when(request.getSession()).thenReturn(session);
-        when(request.getRequestDispatcher("index.jsp")).thenReturn(rd);
+        when(request.getRequestDispatcher("/index.jsp")).thenReturn(rd);
 
-        new LogoutServlet().doPost(request, response);
+        new LogoutServlet().doGet(request, response);
 
         session.invalidate();
 
