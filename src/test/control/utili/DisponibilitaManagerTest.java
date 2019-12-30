3305096251d0d9702f88d0801522d4c4b37e2fbd
antonio.decaro <antonio.decaro@outlook.it>
2019-12-30 13:59:02 +0100

    Inserito il testing WhiteBox della classe DisponibilitaManager

--- src/test/control/utili/DisponibilitaManagerTest.java
+++ src/test/control/utili/DisponibilitaManagerTest.java
@@ -1,27 +1,35 @@
 package control.utili;
 
 import model.dao.PrenotazioneDAO;
-import model.pojo.Aula;
+import model.database.StubPrenotazioneDAO;
+import model.pojo.*;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.sql.Date;
+import java.sql.Time;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class DisponibilitaManagerTest {
 
-    @Mock
-    private PrenotazioneDAO mockPrenotazioneDAO;
+    private PrenotazioneDAO prenotazioneDAO;
+    private static Edificio mockEdifcio;
+    private static Utente mockUtente;
+
+    @BeforeAll
+    static void beforeAll() {
+        mockEdifcio = new Edificio("Mock");
+        mockUtente = new Utente();
+    }
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.initMocks(this);
+        prenotazioneDAO = new StubPrenotazioneDAO();
     }
 
     @AfterEach
@@ -29,16 +37,170 @@ class DisponibilitaManagerTest {
     }
 
     @Test
-    void testConstructor() throws Exception {
+    void testIsAulaDisponibile1() throws Exception {
+        String disp = Files.readString(Paths.get("./src/test/resources/DisponibiltaManagerRes/aula1.json"));
+        Aula aula = new Aula("Aula1", 50, 100, disp, mockEdifcio);
+        Date data = Date.valueOf("2019-12-30");
+        Time oraInizio = Time.valueOf("11:00:00");
+        Time oraFine = Time.valueOf("12:00:00");
+        DisponibilitaManager disponibilitaManager = new DisponibilitaManager(aula, prenotazioneDAO);
+        assertTrue(disponibilitaManager.isAulaDisponibile(data, oraInizio, oraFine));
+    }
 
-        String jsonString = Files.readString(Paths.get("./src/test/resources/TC_3/disp_aulaP3.json"));
-        Aula aula = new Aula();
-        aula.setDisponibilita(jsonString);
-        DisponibilitaManager disponibilitaManager = new DisponibilitaManager(aula, mockPrenotazioneDAO);
-        final String expected = "[[[10:30, 15:30], [16:00, 17:00]], [[11:00, 12:30], [15:00, 18:30]], [[10:30, 15:30], " +
-                "[16:00, 17:00]], [[10:30, 15:30], [16:00, 17:00]], [[10:30, 15:30], [16:00, 17:00]], [[10:30, 15:30], " +
-                "[16:00, 17:00]], [[10:30, 15:30], [16:00, 17:00]]]";
-        System.out.println(Arrays.toString(disponibilitaManager.getDisponibilita().intervalli));
-        assertEquals(expected, Arrays.toString(disponibilitaManager.getDisponibilita().intervalli));
+    @Test
+    void testIsAulaDisponibile2() throws Exception {
+        String disp = Files.readString(Paths.get("./src/test/resources/DisponibiltaManagerRes/aula1.json"));
+        Aula aula = new Aula("Aula1", 50, 100, disp, mockEdifcio);
+        Date data = Date.valueOf("2019-12-30");
+        Time oraInizio = Time.valueOf("09:00:00");
+        Time oraFine = Time.valueOf("10:00:00");
+        DisponibilitaManager disponibilitaManager = new DisponibilitaManager(aula, prenotazioneDAO);
+        assertFalse(disponibilitaManager.isAulaDisponibile(data, oraInizio, oraFine));
+    }
+
+    @Test
+    void testIsAulaDisponibile3() throws Exception {
+        String disp = Files.readString(Paths.get("./src/test/resources/DisponibiltaManagerRes/aula1.json"));
+        Aula aula = new Aula("Aula1", 50, 100, disp, mockEdifcio);
+        Date data = Date.valueOf("2019-12-31");
+        Time oraInizio = Time.valueOf("09:00:00");
+        Time oraFine = Time.valueOf("10:00:00");
+        DisponibilitaManager disponibilitaManager = new DisponibilitaManager(aula, prenotazioneDAO);
+        assertTrue(disponibilitaManager.isAulaDisponibile(data, oraInizio, oraFine));
+    }
+
+    @Test
+    void testIsAulaDisponibile4() throws Exception {
+        String disp = Files.readString(Paths.get("./src/test/resources/DisponibiltaManagerRes/aula1.json"));
+        Aula aula = new Aula("Aula1", 50, 100, disp, mockEdifcio);
+        Date data = Date.valueOf("2019-12-31");
+        Time oraInizio = Time.valueOf("17:00:00");
+        Time oraFine = Time.valueOf("19:00:00");
+        DisponibilitaManager disponibilitaManager = new DisponibilitaManager(aula, prenotazioneDAO);
+        assertFalse(disponibilitaManager.isAulaDisponibile(data, oraInizio, oraFine));
+    }
+
+    @Test
+    void testIsAulaDisponibile5() throws Exception {
+        String disp = Files.readString(Paths.get("./src/test/resources/DisponibiltaManagerRes/aula1.json"));
+        Aula aula = new Aula("Aula1", 50, 100, disp, mockEdifcio);
+        Date data = Date.valueOf("2019-12-31");
+        Time oraInizio = Time.valueOf("17:00:00");
+        Time oraFine = Time.valueOf("19:00:00");
+        DisponibilitaManager disponibilitaManager = new DisponibilitaManager(aula, prenotazioneDAO);
+        assertFalse(disponibilitaManager.isAulaDisponibile(data, oraInizio, oraFine));
+    }
+
+    @Test
+    void testIsAulaDisponibile6() throws Exception {
+        String disp = Files.readString(Paths.get("./src/test/resources/DisponibiltaManagerRes/aula1.json"));
+        Aula aula = new Aula("Aula1", 50, 100, disp, mockEdifcio);
+        Date data = Date.valueOf("2019-12-30");
+        Time oraInizio = Time.valueOf("11:00:00");
+        Time oraFine = Time.valueOf("13:00:00");
+        Prenotazione p1 = new Prenotazione(101, Date.valueOf("2019-12-30"),
+                Time.valueOf("13:00:00"), Time.valueOf("14:00:00"),
+                TipoPrenotazione.AULA, aula, mockUtente);
+        prenotazioneDAO.insert(p1);
+        DisponibilitaManager disponibilitaManager = new DisponibilitaManager(aula, prenotazioneDAO);
+        assertTrue(disponibilitaManager.isAulaDisponibile(data, oraInizio, oraFine));
+    }
+
+    @Test
+    void testIsAulaDisponibile7() throws Exception {
+        String disp = Files.readString(Paths.get("./src/test/resources/DisponibiltaManagerRes/aula1.json"));
+        Aula aula = new Aula("Aula1", 50, 100, disp, mockEdifcio);
+        Date data = Date.valueOf("2019-12-30");
+        Time oraInizio = Time.valueOf("11:00:00");
+        Time oraFine = Time.valueOf("13:30:00");
+        Prenotazione p1 = new Prenotazione(101, Date.valueOf("2019-12-30"),
+                Time.valueOf("13:00:00"), Time.valueOf("14:00:00"),
+                TipoPrenotazione.AULA, aula, mockUtente);
+        prenotazioneDAO.insert(p1);
+        DisponibilitaManager disponibilitaManager = new DisponibilitaManager(aula, prenotazioneDAO);
+        assertFalse(disponibilitaManager.isAulaDisponibile(data, oraInizio, oraFine));
+    }
+
+    @Test
+    void testIsAulaDisponibile8() throws Exception {
+        String disp = Files.readString(Paths.get("./src/test/resources/DisponibiltaManagerRes/aula1.json"));
+        Aula aula = new Aula("Aula1", 50, 100, disp, mockEdifcio);
+        Date data = Date.valueOf("2019-12-30");
+        Time oraInizio = Time.valueOf("11:00:00");
+        Time oraFine = Time.valueOf("13:30:00");
+        Prenotazione p1 = new Prenotazione(101, Date.valueOf("2019-12-30"),
+                Time.valueOf("13:00:00"), Time.valueOf("14:00:00"),
+                TipoPrenotazione.POSTO, aula, mockUtente);
+        prenotazioneDAO.insert(p1);
+        DisponibilitaManager disponibilitaManager = new DisponibilitaManager(aula, prenotazioneDAO);
+        assertTrue(disponibilitaManager.isAulaDisponibile(data, oraInizio, oraFine));
+    }
+
+    @Test
+    void testIsAula_InManutenzione_Disponibile() throws Exception {
+        String disp = Files.readString(Paths.get("./src/test/resources/DisponibiltaManagerRes/aula2.json"));
+        Aula aula = new Aula("Aula1", 50, 100, disp, mockEdifcio);
+        Date data = Date.valueOf("2019-12-30");
+        Time oraInizio = Time.valueOf("11:00:00");
+        Time oraFine = Time.valueOf("13:30:00");
+        DisponibilitaManager disponibilitaManager = new DisponibilitaManager(aula, prenotazioneDAO);
+        assertFalse(disponibilitaManager.isAulaDisponibile(data, oraInizio, oraFine));
+    }
+
+    @Test
+    void testIsAulaDisponibileThrows() throws Exception {
+        String disp = Files.readString(Paths.get("./src/test/resources/DisponibiltaManagerRes/aula1.json"));
+        Aula aula = new Aula("Aula1", 50, 100, disp, mockEdifcio);
+        Date data = Date.valueOf("2019-12-30");
+        Time oraInizio = Time.valueOf("11:00:00");
+        Time oraFine = Time.valueOf("10:00:00");
+        DisponibilitaManager disponibilitaManager = new DisponibilitaManager(aula, prenotazioneDAO);
+        assertThrows(IllegalArgumentException.class, () ->
+                disponibilitaManager.isAulaDisponibile(data, oraInizio, oraFine),
+                "Il parametro oraFine non può essere antecedente a oraInizio");
+    }
+
+    @Test
+    void testIsAulaDisponibileTuttiPostiOccupati() throws Exception {
+        String disp = Files.readString(Paths.get("./src/test/resources/DisponibiltaManagerRes/aula1.json"));
+        Aula aula = new Aula("Aula1", 100, 100, disp, mockEdifcio);
+        Date data = Date.valueOf("2019-12-31");
+        Time oraInizio = Time.valueOf("11:00:00");
+        Time oraFine = Time.valueOf("13:00:00");
+        DisponibilitaManager disponibilitaManager = new DisponibilitaManager(aula, prenotazioneDAO);
+        assertTrue(disponibilitaManager.isAulaDisponibile(data, oraInizio, oraFine));
+    }
+
+    @Test
+    void testIsPostoDisponibile1() throws Exception {
+        String disp = Files.readString(Paths.get("./src/test/resources/DisponibiltaManagerRes/aula1.json"));
+        Aula aula = new Aula("Aula1", 99, 100, disp, mockEdifcio);
+        Date data = Date.valueOf("2019-12-30");
+        Time oraInizio = Time.valueOf("11:00:00");
+        Time oraFine = Time.valueOf("13:30:00");
+        DisponibilitaManager disponibilitaManager = new DisponibilitaManager(aula, prenotazioneDAO);
+        assertTrue(disponibilitaManager.isPostoDisponibile(data, oraInizio, oraFine));
+    }
+
+    @Test
+    void testIsPostoDisponibile2() throws Exception {
+        String disp = Files.readString(Paths.get("./src/test/resources/DisponibiltaManagerRes/aula1.json"));
+        Aula aula = new Aula("Aula1", 100, 100, disp, mockEdifcio);
+        Date data = Date.valueOf("2019-12-30");
+        Time oraInizio = Time.valueOf("11:00:00");
+        Time oraFine = Time.valueOf("13:30:00");
+        DisponibilitaManager disponibilitaManager = new DisponibilitaManager(aula, prenotazioneDAO);
+        assertFalse(disponibilitaManager.isPostoDisponibile(data, oraInizio, oraFine));
+    }
+
+    @Test
+    void testIsPostoDisponibile3() throws Exception {
+        String disp = Files.readString(Paths.get("./src/test/resources/DisponibiltaManagerRes/aula2.json"));
+        Aula aula = new Aula("Aula1", 80, 100, disp, mockEdifcio);
+        Date data = Date.valueOf("2019-12-30");
+        Time oraInizio = Time.valueOf("11:00:00");
+        Time oraFine = Time.valueOf("13:30:00");
+        DisponibilitaManager disponibilitaManager = new DisponibilitaManager(aula, prenotazioneDAO);
+        assertFalse(disponibilitaManager.isPostoDisponibile(data, oraInizio, oraFine));
     }
 }
